61a541fa5001e6533002152d939cedd17de6e81a
ashish-panicker <ashish.s.panicker@gmail.com>
2019-10-01 09:25:53 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch:'full', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
